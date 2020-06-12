adafb797711f280ebe46fa90831316b19f7471aa
Rahul <rahuldev1997@yahoo.com>
2020-06-12 19:49:37 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 style={{ background: "#282C34", color: "#FFF", padding: "20px"}}>
+      <h2 style={{ background: "#282C34", color: "#FFF", padding: "20px"}}>
         {" "}
         Welcome to Rahul's WebRTC App
-      </h1>
+      </h2>
     </div>
   );
 }
